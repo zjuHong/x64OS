1c40a514ee66bb78ff69f3352106270b514700d4
hyd <21825086@zju.edu.cn>
2020-12-09 22:48:39 +0800

    loader

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 nasm boot.asm -o boot.bin
+nasm loader.asm -o loader.bin
 dd if=boot.bin of=./boot.img bs=512 count=1 conv=notrunc
+sudo mount boot.img /media/ -t vfat -o loop
+sudo cp loader.bin /media/
+sync
+sudo umount /media/
 bochs -f bochsconfig
+
