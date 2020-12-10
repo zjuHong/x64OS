5306ac03c011843442160002eacf66c4f3dd9b73
hyd <21825086@zju.edu.cn>
2020-12-10 20:38:40 +0800

    kernel

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
-nasm boot.asm -o boot.bin
-nasm loader.asm -o loader.bin
-dd if=boot.bin of=./boot.img bs=512 count=1 conv=notrunc
+dd if=./bootloader/boot.bin of=./boot.img bs=512 count=100 conv=notrunc
 sudo mount boot.img /media/ -t vfat -o loop
-sudo cp loader.bin /media/
+sudo cp ./bootloader/loader.bin /media/
+sudo cp ./kernel/kernel.bin /media/
 sync
 sudo umount /media/
 bochs -f bochsconfig
