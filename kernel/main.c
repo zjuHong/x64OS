677a4f9e4d56c3978eeeea5c3c8d863cabfe1e2f
hyd <21825086@zju.edu.cn>
2020-12-11 22:47:42 +0800

    printk

--- kernel/main.c
+++ kernel/main.c
@@ -13,11 +13,25 @@
 *
 ***************************************************/
 
+#include "lib.h"
+#include "printk.h"
+
 void Start_Kernel(void)
 {
 	int *addr = (int *)0xffff800000a00000;
 	int i;
 
+	Pos.XResolution = 1440;
+	Pos.YResolution = 900;
+
+	Pos.XPosition = 0;
+	Pos.YPosition = 5;
+
+	Pos.XCharSize = 8;
+	Pos.YCharSize = 16;
+
+	Pos.FB_addr = (int *)0xffff800000a00000;
+	Pos.FB_length = (Pos.XResolution * Pos.YResolution * 4);
 
 	for(i = 0 ;i<1440*20;i++)
 	{
@@ -51,6 +65,8 @@ void Start_Kernel(void)
 		*((char *)addr+3)=(char)0x00;	
 		addr +=1;	
 	}
+
+	color_printk(YELLOW,BLACK,"Hello\t\t World!\n");
 
 	while(1)
 		;
