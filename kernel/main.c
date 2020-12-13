b73438ebc57f2471a66f82fc51add9b1f0190fb3
hyd <21825086@zju.edu.cn>
2020-12-13 17:12:34 +0800

    interrupt

--- kernel/main.c
+++ kernel/main.c
@@ -18,6 +18,7 @@
 #include "gate.h"
 #include "trap.h"
 #include "memory.h"
+#include "interrupt.h"
 
 /*
 		static var 
@@ -34,8 +35,6 @@ void Start_Kernel(void)
 {
 	int *addr = (int *)0xffff800000a00000;
 	int i;
-
-	struct Page * page = NULL;
 
 	Pos.XResolution = 1440;
 	Pos.YResolution = 900;
@@ -63,20 +62,8 @@ void Start_Kernel(void)
 	color_printk(RED,BLACK,"memory init \n");
 	init_memory();
 
-	color_printk(RED,BLACK,"memory_management_struct.bits_map:%#018lx\n",*memory_management_struct.bits_map);
-	color_printk(RED,BLACK,"memory_management_struct.bits_map:%#018lx\n",*(memory_management_struct.bits_map + 1));
-
-	page = alloc_pages(ZONE_NORMAL,64,PG_PTable_Maped | PG_Active | PG_Kernel);
-
-	for(i = 0;i <= 64;i++)
-	{
-		color_printk(INDIGO,BLACK,"page%d\tattribute:%#018lx\taddress:%#018lx\t",i,(page + i)->attribute,(page + i)->PHY_address);
-		i++;
-		color_printk(INDIGO,BLACK,"page%d\tattribute:%#018lx\taddress:%#018lx\n",i,(page + i)->attribute,(page + i)->PHY_address);
-	}
-
-	color_printk(RED,BLACK,"memory_management_struct.bits_map:%#018lx\n",*memory_management_struct.bits_map);
-	color_printk(RED,BLACK,"memory_management_struct.bits_map:%#018lx\n",*(memory_management_struct.bits_map + 1));
+	color_printk(RED,BLACK,"interrupt init \n");
+	init_interrupt();
 
 	while(1)
 		;
